5cb913f3ba424ecda12361311e19c4c8a9fe3cad
Lia Tazetdinova <davaispi@list.ru>
2024-06-01 17:58:41 +0300

    start page

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,7 @@
 import '../css/style.css';
+import { darkModeHandle } from './utils';
+
+darkModeHandle();
+
+const startGameButton = document.getElementById('startGame');
+startGameButton.addEventListener('click', () => {});
